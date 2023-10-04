659b2b961c9adfa79c55959c63a216eedfa4a4d5
cipher <sut-v-lubvi@yandex.ru>
2023-10-04 17:48:17 +0300

    adfsgerdg

--- src/app/addTest/page.tsx
+++ src/app/addTest/page.tsx
@@ -1,12 +1,5 @@
 "use client";
 import TestForm from "@/widgets/AddTest";
-
 export default function AddTest() {
-  if (localStorage.getItem("password") === "yaplakal2000") return <TestForm />;
-  const password = prompt("Введите пароль");
-  if (password === "yaplakal2000") {
-    localStorage.setItem("password", password);
-    return <TestForm />;
-  }
-  window.location.reload();
+  return <TestForm />;
 }
